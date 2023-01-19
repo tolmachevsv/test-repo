4add4c1c733d5cc7aa0bc1321ffe536bff94f205
Sergey <tolmachevsv96@yandex.ru>
2023-01-19 18:13:15 +0300

    changed google to yandex

--- src/test/java/com/tolmachevsv/tests/YandexTest.java
+++ src/test/java/com/tolmachevsv/tests/YandexTest.java
@@ -2,16 +2,15 @@ package com.tolmachevsv.tests;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byTitle;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class GoogleTest {
+public class YandexTest extends TestBase {
 
     @Test
     public void SimpleTest() {
-        open("https://google.com");
-        $(".gLFyf").setValue("Steam");
-        $(".gLFyf").submit();
+        open("https://ya.ru");
+        $("#text").sendKeys("Steam");
+        $("#text").submit();
     }
 }
